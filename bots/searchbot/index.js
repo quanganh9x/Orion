f6e192d15acc5f4782f9a1df4d0d14864423be0f
Thao <thuthao541998@gmail.com>
2018-05-18 14:24:02 +0700

    sửa api và nhận tên phim

--- bots/searchbot/index.js
+++ bots/searchbot/index.js
@@ -1,10 +1,11 @@
-const SEARCH_API = 'https://www.googleapis.com/customsearch/v1?key=AIzaSyDjeakB1alo42nS2NNR9xjSK5Zyi2yMiSA&cx=017576662512468239146:omuauf_lfve&q=';
+const GOOGLE_API = "https://www.googleapis.com/customsearch/v1?key=AIzaSyDjeakB1alo42nS2NNR9xjSK5Zyi2yMiSA&cx=010163171448574847880:qglriqbcnk4&fields=items(title,link)&hl=vi&q=";
 module.exports = function (bot) {
   bot.hear(['search phim'], (payload, chat) => {
     chat.conversation((convo) => {
       convo.question("Bạn muốn tìm phim gì", (payload, convo) => {
-        var name = payload.messenge.text;       
-        fetch(SEARCH_API + name)
+        var searchName = payload.messenge.text;
+        var newName = name.replace(" ", "+");
+        fetch(GOOGLE_API + "xem+phim+" + newName)
           .then(res => res.json())
           .then(json => {
             chat.say({
@@ -12,7 +13,7 @@ module.exports = function (bot) {
               url: json.data[0].images.fixed_height.url
             });
           });
-        convo.say("Link của thím è");
+        convo.say("Phim bạn tìm nè");
         convo.say(json.data[0].url);
       });
       convo.end(); // trả về thông tin cho người dùng xong thì end conversation
