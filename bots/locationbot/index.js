de3729a4415b255ee36f3c45f383a3f07d8caffb
Tra <trauvmd00621@fpt.edu.vn>
2018-05-18 20:12:26 +0700

    Sửa API thời tiết sang của openweather

--- bots/locationbot/index.js
+++ bots/locationbot/index.js
@@ -1,4 +1,6 @@
+var WEATHER_API = 'api.openweathermap.org/data/2.5/weather?';
 
+/////////////////////////////// Thời tiết bot ////////////////////////////
 module.exports = function (bot) {
     bot.hear(['weather'], (payload, chat) => {
         chat.conversation((convo) => {
@@ -7,23 +9,30 @@ module.exports = function (bot) {
                     var lat = payload.message.attachments[0].payload.coordinates.lat;
                     var long = payload.message.attachments[0].payload.coordinates.long;
 
-                    var query = new YQL('select * from weather.forecast where text="(' + lat + ',' + long + ')"');
-                    query.exec(function(err, data) {
-                      var location = data.query.results.channel.location;
-                      var condition = data.query.results.channel.item.condition;
-                      convo.set('địa điểm', location);
-                      convo.set('thời tiết', condition);
-                      convo.say("Oki kết quả");
-                        if (condition.temp > 110) {
-                           convo.say("Trái đất tiêu rùi");
-                           convo.sendAttachment('image', 'https://media1.tenor.com/images/71b3af3541f226215a579fb96386fc35/tenor.gif?itemid=4808980');
-                        } else if (condition.temp < 0) {
-                            convo.say("Kỉ băng hà ?");
-                        } else convo.say("Thời tiết hôm nay ở " + location + " là " + condition.temp + data.query.results.channel.units.temperature);
-                    });
+                    fetch(WEATHER_API + "lat=" + lat + "&lon=" + long)
+                      .then(
+                        function(response) {
+                            if (response.status !== 200) {
+                                console.log('Lỗi??? ' + response.status);
+                                return;
+                            }                           
+                            response.json().then(function(data) {
+                                var location = data.name;
+                                var tempF = data.main.temp;
+                                var tempC = (tempF - 33.8)/1.8;
+                                var condition = data.weather.description;
+                                if (tempC > 45) {
+                                   convo.say("Trái đất tiêu rùi");
+                                   convo.sendAttachment('image', 'https://media1.tenor.com/images/71b3af3541f226215a579fb96386fc35/tenor.gif?itemid=4808980');
+                                } else if (tempC < -40) {
+                                    convo.say("Kỉ băng hà ?");
+                                } else convo.say("Thời tiết bây giờ ở " + location + " là " + tempC + "oC, " + tempF + "oF.");
+                            });
+                        }
+                    );
                 }
             });
-            convo.say("Bản tin thời tiết tới đây là kết thúc, xin nhường chỗ cho các chương trình khác.");
+            convo.say("Bản tin thời tiết tới đây là kết thúc, xin nhường chỗ cho các chương trình khác~");
             convo.end(); // trả về thông tin cho người dùng xong thì end conversation
         });
     });
