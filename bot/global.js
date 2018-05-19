b1bfb88d0c7aa8ff7cc628c77809329b70d1e4c4
trungdo1410 <trungdqd00630@fpt.edu.vn>
2018-05-19 15:39:35 +0700

    Deleted unuse folder

--- bot/global.js
+++
@@ -1,9 +0,0 @@
-const config = require('./config');
-///// bot modules /////
-let helloBot = require('./hellobot/index');
-///////////////////////
-
-module.exports = function (bot) {
-    config(bot);
-    helloBot(bot);
-};
