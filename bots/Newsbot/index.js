f32ee28eb70399e70118f524623dce977a876d00
trungdo1410 <trungdqd00630@fpt.edu.vn>
2018-05-19 15:42:47 +0700

    recoding news bot

--- bots/Newsbot/index.js
+++ bots/Newsbot/index.js
@@ -1,32 +1,14 @@
-var request = require('request');
-var cheerio = require('cheerio');
-var URL = require('url-parse');
-var vnExpress = "https://vnexpress.net";
+const Parser = require('rss-parser');
+const parser = new Parser();
+const VNExpress_URL = 'https://vnexpress.net/rss/tin-moi-nhat.rss';
 
-console.log("Ban dang doc bao tai trang " + vnExpress);
-request(vnExpress, function(err, response, body) {
-	if (err){
-		console.log("Error: " + err);
-	}
-
-	   if(response.statusCode === 200) {
-     // Parse the document body
-     var $ = cheerio.load(body);
-     for (var i = 0; i < 5; i++) {
-     	console.log("Tin moi nhat :  " + $('.list_news').slice(i).text());
-     }
-     var news = "Tin moi nhat :  " + $('.list_news').slice(i).text()
-   }
-});
-
-module.exports = function (bot) {
-	bot.hear(['tin tức', 'đọc báo', 'tin mới'], (payload, chat) => {
-  chat.getUserProfile().then((user) => {
-    chat.say(`Hello, ${user.first_name}!`).then.then(() => {
-    	chat.say({
-    		text: news
-
-    	});
-  });
-});
-}
+module.exports = function (chat) {
+    (async () => {
+        let feed = await parser.parseURL(VNExpress_URL);
+        chat.say("Tin được lấy từ: " + feed.title).then(() => {
+            for (let i = 0; i < 5; i++) {
+                chat.say(feed.items[i].title + '\n' + feed.items[i].link);
+            }
+        });
+    })();
+};
