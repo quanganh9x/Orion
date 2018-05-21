9571b758605213c253fc6207337ca1c95f6fd3e0
trungdo1410 <trungdqd00630@fpt.edu.vn>
2018-05-22 06:33:33 +0700

    recoding newsbot

--- bots/newsbot/index.js
+++ bots/newsbot/index.js
@@ -1,7 +1,9 @@
 const vnexpress = require('./vnexpress');
+const tinmoi = require('./tinmoi');
 
 module.exports = function (bot) {
     bot.hear(['tin tức', 'đọc báo', 'tin mới', 'news'], (payload, chat) => {
             vnexpress(chat);
+            tinmoi(chat);
     });
 };
