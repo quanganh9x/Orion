4c16a0da97a641288006dff2d1041a23b1986387
quanganh9x <quanganhpro9x@gmail.com>
2018-05-25 11:56:17 +0700

    Optimized locationbot. Add method to change location

--- bots/locationbot/index.js
+++ bots/locationbot/index.js
@@ -6,24 +6,51 @@ module.exports = function (bot) {
         chat.conversation((convo) => {
             (async () => {
                 await convo.say("[LocationBOT] v1.0 xin chào mừng");
-                await convo.ask({
-                    text: "Bạn ở đâu thế - gửi vị trí lên để mình xử lý nhen ?",
-                    quickReplies: {
-                        content_type: "location"
-                    }
-                }, (payload, convo) => {
-                    if (payload.message.attachments[0].payload.coordinates) {
-                        convo.set('lat', payload.message.attachments[0].payload.coordinates.lat);
-                        convo.set('long', payload.message.attachments[0].payload.coordinates.long);
-                        convo.say("Thành công!")
-                    } else {
-                        convo.say(":( Mình chưa tìm được vị trí rồi");
-                        convo.end();
-                    }
-                });
+                const askLocation = (convo) => {
+                    convo.ask({
+                        text: "Bạn ở đâu thế - gửi vị trí lên để mình xử lý nhen ?",
+                        quickReplies: {
+                            content_type: "location"
+                        }
+                    }, (payload, convo) => {
+                        if (payload.message.attachments[0].payload.coordinates) {
+                            convo.set('lat', payload.message.attachments[0].payload.coordinates.lat);
+                            convo.set('long', payload.message.attachments[0].payload.coordinates.long);
+                            convo.say("Thành công!")
+                        } else {
+                            convo.say(":( Mình chưa tìm được vị trí rồi");
+                            convo.end();
+                        }
+                    });
+                };
+                await askLocation(convo);
                 const locationbot = (convo) => {
-                    convo.ask(() => {}, )
-                }
+                    convo.ask(() => {}, (payload, convo) => {
+                        (async () => {
+                            switch (payload.message.text) {
+                                case 'thoi tiet':
+                                    await weather(convo.get('lat'), convo.get('long'), convo);
+                                    break;
+                                case 'dia diem':
+                                    break;
+                                case 'doi':
+                                    await askLocation(convo);
+                                    break;
+                                case 'whereami':
+                                    await convo.say("Main > LocationBOT");
+                                    break;
+                                case 'end':
+                                    convo.end();
+                                    break;
+                                default:
+                                    convo.say("Không có tuỳ chọn này :( Ý bạn là \\'thoi tiet\\' hoặc \\'dia diem\\' ?\"");
+                                    break;
+                            }
+                            locationbot(convo);
+                        })();
+                    })
+                };
+                locationbot(convo);
             })();
         });
     });
