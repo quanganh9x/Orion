96043d86fc5e3d68cf9f250b87de756926690f63
Thao <thuthao541998@gmail.com>
2018-05-28 21:07:06 +0700

    sửa 1 xíu

--- bots/converterbot/currency-convert.js
+++ bots/converterbot/currency-convert.js
@@ -5,7 +5,7 @@ module.exports = (bot) => {
         chat.conversation((convo) => {
       		(async ()=>{
             await convo.say("Đây là các đồng tiền thông dụng");
-            await convo.say("1.Đô la Mỹ : USD \n 2.Việt Nam Đồng : VND \n 3. Yên Nhật : JPY \n 4.Bảng Anh : EUR \n 5. Đô Úc : AUD \n 6. Nhân dân tệ : CNY \n 7. Baht Thái : THB \n 8. Won Hàn Quốc : KRW \n 9. Đô Singapo : SGD \n 10. Đô la Hồng Công");                  
+            await convo.say("1.Đô la Mỹ : USD \n 2.Việt Nam Đồng : VND \n 3. Yên Nhật : JPY \n 4.Bảng Anh : EUR \n 5. Đô Úc : AUD \n 6. Nhân dân tệ : CNY \n 7. Baht Thái : THB \n 8. Won Hàn Quốc : KRW \n 9. Đô Singapo : SGD \n 10. Đô la Hồng Công : HKD");                  
       			await convo.say("Bạn muốn đổi từ đồng tiền nào?").then(() => {
                 	convo.set("from", payload.message.text);
             	});
