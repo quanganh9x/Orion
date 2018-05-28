0f7f400930a7ed8bdebbc9fb8e9c14fc086674b1
Tra <trauvmd00621@fpt.edu.vn>
2018-05-28 22:30:18 +0700

    Sửa convo.say -> ask

--- bots/converterbot/index.js
+++ bots/converterbot/index.js
@@ -4,18 +4,18 @@ module.exports = (bot) => {
     bot.hear('convert', (payload, chat) => {
         chat.conversation((convo) => {
       		(async ()=>{
-      			await convo.say("Bạn muốn đổi từ đơn vị nào?").then(() => {
-                	convo.set("from", payload.message.text);
-            	});
-            	await convo.say("Và sang đơn vị ...?").then(() => {
-                	convo.set("to", payload.message.text);
-            	});
-            	await convo.say("Ok. Đổi từ bao nhiêu sang nhỉ?").then(() => {
-                	convo.set("num", payload.message.text);
-            	});
-            	await convo.say("Ok của bạn đây: " + parseInt(num) + from + " bằng " + convert(parseInt(num)).from(from).to('to') + to + " nhé~");
-            	convo.say("<3");
-            	convo.end();
+      			await convo.ask("Bạn muốn đổi từ đơn vị nào?", (payload, convo) => {
+              	convo.set("from", payload.message.text);
+          	});
+          	await convo.ask("Và sang đơn vị ...?", (payload, convo) => {
+              	convo.set("to", payload.message.text);
+          	});
+          	await convo.ask("Ok. Đổi từ bao nhiêu sang nhỉ?", (payload, convo) => {
+              	convo.set("num", payload.message.text);
+          	});
+          	await convo.say("Ok của bạn đây: " + parseInt(num) + from + " bằng " + convert(parseInt(num)).from(from).to('to') + to + " nhé~");
+          	convo.say("<3");
+          	convo.end();
       		});
       	});
     });
