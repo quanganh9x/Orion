9b46a9bf7e3e4eb89b7a7e91c2fc2821bb137620
Thao <thuthao541998@gmail.com>
2018-05-28 21:59:44 +0700

    xóa await

--- bots/converterbot/currency-convert.js
+++ bots/converterbot/currency-convert.js
@@ -1,12 +1,14 @@
 var money = require("money");
 
-module.exports = (from, to, num , convo) => {
+module.exports = (from, to, num, convo) => {
   chat.conversation((convo) => {
-    (async () => {           
-      await convo.say("Ok của bạn đây: " + parseInt(num) + from + " bằng " + money(parseInt(num)).from(from).to(to) + to + " nhé~");
-      convo.say("<3");
-      convo.end();
-    });
+    if (from == undefined) {
+      convo.say("Ok của bạn đây: " + money(num).to(to) + to + " nhé~");
+    }
+    await convo.say("Ok của bạn đây: " + parseInt(num) + from + " bằng " + money(parseInt(num)).from(from).to(to) + to + " nhé~");
+    convo.say("<3");
+    convo.end();
+
   });
 
 };
--- bots/converterbot/index.js
+++ bots/converterbot/index.js
@@ -6,33 +6,33 @@ module.exports = function (bot) {
       convo.say("[ConvertBOT] v1.0 xin chào mừng!").then(() => {
         const searchbot = (convo) => {
           convo.ask(() => { }, (payload, convo) => {
-            (async () => {
-              switch (payload.message.text) {
-                case 'tiền':
-                  await convo.ask("Đây là các đồng tiền thông dụng", (payload, convo) => {
-                    await convo.say("1.Đô la Mỹ : USD \n 2.Việt Nam Đồng : VND \n 3. Yên Nhật : JPY \n 4.Bảng Anh : EUR \n 5. Đô Úc : AUD \n 6. Nhân dân tệ : CNY \n 7. Baht Thái : THB \n 8. Won Hàn Quốc : KRW \n 9. Đô Singapo : SGD \n 10. Đô la Hồng Công : HKD");
-                    await convo.ask("Bạn muốn đổi từ đồng tiền nào?", (payload, convo) => {
-                      convo.set("from", payload.message.text);
-                      await convo.ask("Và sang đồng tiền ...?", (payload, convo) => {
-                        convo.set("to", payload.message.text);
-                        await convo.ask("Ok. Đổi từ bao nhiêu sang nhỉ?", (payload, convo) => {
-                          convo.set("num", payload.message.text);
-                        })
+
+            switch (payload.message.text) {
+              case 'tiền':
+                convo.ask("Đây là các đồng tiền thông dụng", (payload, convo) => {
+                  convo.say("1.Đô la Mỹ : USD \n 2.Việt Nam Đồng : VND \n 3. Yên Nhật : JPY \n 4.Bảng Anh : EUR \n 5. Đô Úc : AUD \n 6. Nhân dân tệ : CNY \n 7. Baht Thái : THB \n 8. Won Hàn Quốc : KRW \n 9. Đô Singapo : SGD \n 10. Đô la Hồng Công : HKD");
+                  convo.ask("Bạn muốn đổi từ đồng tiền nào?", (payload, convo) => {
+                    convo.set("from", payload.message.text);
+                    convo.ask("Và sang đồng tiền ...?", (payload, convo) => {
+                      convo.set("to", payload.message.text);
+                      convo.ask("Ok. Đổi từ bao nhiêu sang nhỉ?", (payload, convo) => {
+                        convo.set("num", payload.message.text);
                       })
-                    });
-                    money(from, to, num, convo);
+                    })
                   });
-                  break;
-                default:
-                  await convo.say("Không có tuỳ chọn này :( Ý bạn là \'tiền\' hoặc ?");
-                  break;
-              }
-              searchbot(convo);
+                  money(from, to, num, convo);
+                });
+                break;
+              default:
+                await convo.say("Không có tuỳ chọn này :( Ý bạn là \'tiền\' hoặc ?");
+                break;
+
+                searchbot(convo);
             })();
-          });
-        };
-        searchbot(convo);
-      });
-    });
+        });
+    };
+    searchbot(convo);
+  });
+});
   });
 };
