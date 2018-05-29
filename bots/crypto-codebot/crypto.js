1f62313a1699058d3041c2b7f5e81ec312e764c4
Thao <thuthao541998@gmail.com>
2018-05-29 21:55:21 +0700

    sửa npmjs

--- bots/crypto-codebot/crypto.js
+++ bots/crypto-codebot/crypto.js
@@ -1,8 +1,30 @@
-var cryptoBrowserify = require("crypto-browserify");
+var crysptoJS = require('crypto-js');
+// var md5 = require('crypto-js/md5');
+// var sha1 = require('crypto-js/sha1');
+// var sha256 = require('crypto-js/sha256');
+// var sha224 = require('crypto-js/sha224');
+// var sha512 = require('crypto-js/sha512');
+// var sha384 = require('crypto-js/sha384');
+// var sha3 = require('crypto-js/sha3');
+// var ripemd160 = require('crypto-js/ripemd160');
 
-module.exports = (buf,from, convo) => {
-  var hash = cryptoBrowserify.createHash(from,buf);
-  hash.write();
-  hash.end()
-  hash.read();
+module.exports = (buf, from, convo) => {
+  var hashMD5 = crysptoJS.md5(buf);
+  var hashSHA1 = crysptoJS.sha1(buf);
+  var hashSHA256 = crysptoJS.SHA256(buf);
+  var hashSHA224 = crysptoJS.SHA224(buf);
+  var hashSHA512 = crysptoJS.SHA512(buf);
+  var hashSHA384 = crysptoJS.SHA384(buf);
+  var hashSHA3 = crysptoJS.SHA3(buf);
+  var hashRIPEMD160 = crysptoJS.RIPEMD160(buf);
+  convo.say("MD5 : " + hashMD5.toString(CryptoJS.enc.Hex) +
+          "\n SHA1 :  " + hashSHA1.toString(CryptoJS.enc.Hex) +
+          "\n SHA256 :  " + hashSHA256.toString(CryptoJS.enc.Hex) +
+          "\n SHA224 :  " + hashSHA224.toString(CryptoJS.enc.Hex) +
+          "\n SHA512 :  " + hashSHA512.toString(CryptoJS.enc.Hex) +
+          "\n SHA384 :  " + hashSHA384.toString(CryptoJS.enc.Hex) +
+          "\n SHA3 :  " + hashSHA3.toString(CryptoJS.enc.Hex) +
+          "\n RIPEMD160 :  " + hashRIPEMD160.toString(CryptoJS.enc.Hex) +
+          
+  ); 
 };
--- bots/crypto-codebot/index.js
+++ bots/crypto-codebot/index.js
@@ -3,23 +3,23 @@ import { Buffer } from 'buffer';
 const crypto = require('./crypto');
 
 module.exports = function (bot) {
-  bot.hear(['crypto','mã hóa','code'], (payload, chat) => {
-      chat.conversation((convo) => {
+    bot.hear(['crypto', 'mã hóa', 'code'], (payload, chat) => {
+        chat.conversation((convo) => {
             convo.say("[Crypto&CodeBOT] v1.0 xin chào mừng!").then(() => {
                 const cryptocodebot = (convo) => {
-                    convo.ask(() => {}, (payload, convo) => {
+                    convo.ask(() => { }, (payload, convo) => {
                         (async () => {
                             switch (payload.message.text) {
                                 case 'crypto':
-                                    await convo.ask("Bạn muốn mã hóa sang gì (sha1, sha224, sha256, sha384, sha512, md5, rmd160)", (payload, convo) => {
-                                        convo.set("from",payload.message.text);
-                                        convo.ask("Hãy gửi cái bạn muốn mã hóa", (payload, convo) => {                                                                                       
+                                    await convo.ask("Hãy gửi cái bạn muốn mã hóa", (payload, convo) => {
+                                        convo.set("from", payload.message.text);
+                                        convo.ask("", (payload, convo) => {
                                             var buf = new Buffer.from(payload.message.attachment.payload);
                                         })
-                                        crypto(buf,from,convo);
+                                        crypto(buf, from, convo);
                                     });
                                     break;
-                                
+
                                 default:
                                     await convo.say("Không có tuỳ chọn này :( Ý bạn là \'crypto\' hoặc ?");
                                     break;
@@ -30,6 +30,6 @@ module.exports = function (bot) {
                 };
                 cryptocodebot(convo);
             });
-      });
-  });
+        });
+    });
 };
