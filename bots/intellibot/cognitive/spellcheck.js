4a4b37edfbb281f7594bf32d3426e4921ac99b3e
Thao <thuthao541998@gmail.com>
2018-06-01 17:03:44 +0700

    install

--- bots/intellibot/cognitive/spellcheck.js
+++ bots/intellibot/cognitive/spellcheck.js
@@ -3,3 +3,30 @@ const spellcheck = new cognitiveServices.bingSpellCheckV7({
     apiKey: "2ff21eaca8a94c1da719c24e54289d19",
     endpoint: "api.cognitive.microsoft.com"
 });
+const language = require('@google-cloud/language');
+module.exports = (convo, intellibot) => {
+    const client = new language.LanguageServiceClient();
+
+    // The text to analyze
+    const text = 'Hello, world!';
+
+    const document = {
+        content: text,
+        type: 'PLAIN_TEXT',
+    };
+
+    // Detects the sentiment of the text
+    client
+        .analyzeSentiment({ document: document })
+        .then(results => {
+            const sentiment = results[0].documentSentiment;
+
+            console.log(`Text: ${text}`);
+            console.log(`Sentiment score: ${sentiment.score}`);
+            console.log(`Sentiment magnitude: ${sentiment.magnitude}`);
+        })
+        .catch(err => {
+            console.error('ERROR:', err);
+        });
+}
+
