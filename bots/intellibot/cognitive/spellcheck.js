22b3f8c0538df66bba8dcb2fba9a64f1b3912754
Thao <thuthao541998@gmail.com>
2018-06-01 20:42:29 +0700

    sửa

--- bots/intellibot/cognitive/spellcheck.js
+++ bots/intellibot/cognitive/spellcheck.js
@@ -1,10 +1,13 @@
-const cognitiveServices = require('cognitive-services');
-const spellcheck = new cognitiveServices.bingSpellCheckV7({
-    apiKey: "2ff21eaca8a94c1da719c24e54289d19",
-    endpoint: "api.cognitive.microsoft.com"
-});
+// const cognitiveServices = require('cognitive-services');
+// const spellcheck = new cognitiveServices.bingSpellCheckV7({
+//     apiKey: "2ff21eaca8a94c1da719c24e54289d19",
+//     endpoint: "api.cognitive.microsoft.com"
+// });
 const language = require('@google-cloud/language');
-const client = new language.LanguageServiceClient();
+const API_KEY = process.env.API_GOOGLE;
+const client = new language.LanguageServiceClient(
+    keyFilename : API_KEY 
+);
 module.exports = (convo, intellibot) => {
     convo.ask('', (payload, convo) => {
         // The text to analyze
