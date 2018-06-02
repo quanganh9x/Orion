433cbe61b5ebf072e4ece5ba7d075f9d5291d23d
Anh Dang <quanganhpro9x@gmail.com>
2018-06-02 16:09:50 +0700

    Fixed wrong path

--- bots/intellibot/cognitive/computer-vision.js
+++
@@ -1,8 +0,0 @@
-const cognitiveServices = require('cognitive-services');
-const face = new cognitiveServices.computerVision({
-    apiKey: "c7596b44a7534bf38b57f602a8277a33",
-    endpoint: "westcentralus.api.cognitive.microsoft.com"
-});
-
-module.exports = (convo, intellibot) => {
-};
--- bots/intellibot/index.js
+++ bots/intellibot/index.js
@@ -1,53 +1,52 @@
 const vision = require('./cognitive/google-vision');
 const textanalytics = require('./cognitive/textanalytics');
 const spellcheck = require('./cognitive/spellcheck');
-const compvision = require('./cognitive/computer-vision');
 const speech = require('./cognitive/speech');
-const face = require('./cognitive/face');
+const face = require('./cognitive/face/face');
 
 module.exports = function (bot) {
     bot.hear(['intelli', 'ai'], (payload, chat) => {
         chat.conversation((convo) => {
-            convo.say({
-                text: "[IntelliBOT] v1.0. Your own AI accessing system",
-                quickReplies: ['Vision', 'Computer Vision', 'Face', 'TextAnalytics', 'SpellChecking', 'SpeechRecognition']
-            });
-                const intellibot = (convo) => {
-                    convo.ask(() => {}, (payload, convo) => {
-                        switch (payload.message.text) {
-                            case 'Vision':
-                                vision(convo, intellibot);
-                                break;
-                            case 'Computer Vision':
-                                compvision(convo, intellibot);
-                                break;
-                            case 'Face':
-                                face(convo, intellibot);
-                                break;
-                            case 'TextAnalytics':
-                                textanalytics(convo, intellibot);
-                                break;
-                            case 'SpellChecking':
-                                spellcheck(convo, intellibot);
-                                break;
-                            case 'SpeechRecognition':
-                                speech(convo, intellibot);
-                                break;
-                            case 'end':
-                                convo.end();
-                                break;
-                            case 'whereami':
-                                convo.say("Main > IntelliBOT");
-                                intellibot(convo);
-                                break;
-                            default:
-                                convo.say("????");
-                                intellibot(convo);
-                                break;
-                        }
-                    });
-                };
+            const preintellibot = (convo) => {
+                convo.say({
+                    text: "[IntelliBOT] v1.0. Your own AI accessing system",
+                    quickReplies: ['Vision', 'Face', 'TextAnalytics', 'SpellChecking', 'SpeechRecognition']
+                });
                 intellibot(convo);
+            };
+            const intellibot = (convo) => {
+                convo.ask(() => {}, (payload, convo) => {
+                    switch (payload.message.text) {
+                        case 'Vision':
+                            vision(convo, intellibot);
+                            break;
+                        case 'Face':
+                            face(convo, intellibot);
+                            break;
+                        case 'TextAnalytics':
+                            textanalytics(convo, intellibot);
+                            break;
+                        case 'SpellChecking':
+                            spellcheck(convo, intellibot);
+                            break;
+                        case 'SpeechRecognition':
+                            speech(convo, intellibot);
+                            break;
+                        case 'end':
+                            convo.end();
+                            break;
+                        case 'whereami':
+                            convo.say("Main > IntelliBOT");
+                            intellibot(convo);
+                            break;
+                        default:
+                            convo.say("????");
+                            preintellibot(convo);
+                            break;
+                    }
+                });
+            };
+            preintellibot(convo);
         });
     });
 };
