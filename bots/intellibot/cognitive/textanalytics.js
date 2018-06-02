bdf5a4d29448c9cd1f22bceee311caae72b9da50
Thao <thuthao541998@gmail.com>
2018-06-02 15:06:48 +0700

    huhu

--- bots/intellibot/cognitive/textanalytics.js
+++ bots/intellibot/cognitive/textanalytics.js
@@ -1,5 +1,33 @@
-const cognitiveServices = require('cognitive-services');
-const face = new cognitiveServices.textAnalytics({
-    apiKey: "1c0c07e444a3447ba57a600fecd5633b",
-    endpoint: "westcentralus.api.cognitive.microsoft.com"
+
+const language = require('@google-cloud/language');
+const client = new language.LanguageServiceClient({
+  keyFilename: './auth.json'
 });
+module.exports = (convo, intellibot) => {
+  convo.ask('', (payload, convo) => {
+    const text = payload.message.text;
+    const document = {
+      'content': text,
+      'type': 'PLAIN_TEXT',
+    };
+    var request = {
+      'document': document,
+      "encodingType": "UTF8"
+    };
+    client
+      .analyzeSentiment(request)
+      .then(responses => {
+        responses.forEach(element => {
+          console.log('element: ', JSON.stringify(element))
+        });
+      })
+      .catch(err => {
+        console.error('ERROR:', err);
+      });
+  });
+
+}
+
+
+
+
