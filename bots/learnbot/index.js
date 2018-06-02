da6d0edade10a2ae5caadde5026457bb2ef8ac16
Anh Dang <quanganhpro9x@gmail.com>
2018-06-02 13:32:32 +0700

    Fixed wrong path

--- bots/learnbot/index.js
+++ bots/learnbot/index.js
@@ -4,35 +4,37 @@ const ask = require('./ask');
 module.exports = function (bot) {
     bot.hear(['learn'], (payload, chat) => {
         chat.conversation((convo) => {
+            const prelearnbot = (convo) => {
                 convo.say({
                     text: "[LearnBOT] v1.0. Bạn muốn biết gì nè?",
                     quickReplies: ['Từ điển', 'Bách khoa toàn thư']
-                }).then(() => {
-                    const learnbot = (convo) => {
-                        convo.ask(() => {}, (payload, convo) => {
-                            switch (payload.message.text) {
-                                case 'Từ điển':
-                                    tudien(convo, learnbot);
-                                    break;
-                                case 'Bách khoa toàn thư':
-                                    ask(convo, learnbot);
-                                    break;
-                                case 'end':
-                                    convo.end();
-                                    break;
-                                case 'whereami':
-                                    convo.say("Main > LearnBOT");
-                                    learnbot(convo);
-                                    break;
-                                default:
-                                    convo.say("????");
-                                    learnbot(convo);
-                                    break;
-                            }
-                        });
-                    };
-                    learnbot(convo);
                 });
+                learnbot(convo);
+            };
+            const learnbot = (convo) => {
+                convo.ask(() => {}, (payload, convo) => {
+                    switch (payload.message.text) {
+                        case 'Từ điển':
+                            tudien(convo, learnbot);
+                            break;
+                        case 'Bách khoa toàn thư':
+                            ask(convo, learnbot);
+                            break;
+                        case 'end':
+                            convo.end();
+                            break;
+                        case 'whereami':
+                            convo.say("Main > LearnBOT");
+                            learnbot(convo);
+                            break;
+                        default:
+                            convo.say("????");
+                            prelearnbot(convo);
+                            break;
+                    }
+                });
+            };
+            prelearnbot(convo);
         });
     });
 };
