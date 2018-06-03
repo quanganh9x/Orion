bea2010357569401eee230f4510d9524adb24742
quanganh9x <quanganhpro9x@gmail.com>
2018-06-03 18:43:07 +0700

    Fixed ask - LearnBOT

--- bots/learnbot/ask.js
+++ bots/learnbot/ask.js
@@ -1,4 +1,4 @@
-const wolfram = require('wolfram-alpha').createClient("9WTJXY-KEKYY3AGXQ");
+const request = require('request');
 const Translate = require('@google-cloud/translate');
 const translate = new Translate({
     key: process.env.GOOGLE_API_KEY
@@ -9,24 +9,24 @@ module.exports = (convo, learnbot) => {
         translate.translate(payload.message.text, 'en').then(results => {
             results = results[1];
             if (results.data.translations && results.data.translations[0].translatedText) {
-                wolfram.query(results.data.translations[0].translatedText, function (err, result) {
-                    if (err) {
+                request("http://api.wolframalpha.com/v2/query?appid="+process.env.WOLFRAM_API_KEY+"&input="+encodeURIComponent(results.data.translations[0].translatedText)+"&output=json&podindex=2", (err, response, body) => {
+                    if (err || response.statusCode !== 200) {
                         console.log(err);
-                        convo.say("???").then(() => {
-                            learnbot(convo);
-                        });
-                    } else if (result && result[1].subpod.plaintext) {
-                        translate.translate(result[1].subpod.plaintext[0], 'vi').then(response => {
-                            (async () => {
-                                const answer = await response.text;
-                                convo.say("Trả lời: " + answer).then(() => {
-                                    learnbot(convo);
-                                });
-                            })();
-                        }).catch(err => {
-                            console.log(err);
-                            convo.say("???").then(() => learnbot(convo));
-                        });
+                        convo.say("???").then(() => learnbot(convo));
+                    } else {
+                        body = JSON.parse(body);
+                        if (body && body.queryresult.pods.length === 1 && body.queryresult.pods[0].title === "Result" && body.queryresult.pods[0].subpods.length === 1) {
+                            translate.translate(body.queryresult.pods[0].subpods[0].plaintext, 'vi').then(response => {
+                                (async () => {
+                                    response = response[1];
+                                    const answer = await response.data.translations[0].translatedText;
+                                    convo.say("Trả lời: " + answer).then(() => learnbot(convo));
+                                })();
+                            }).catch(err => {
+                                console.log(err);
+                                convo.say("???").then(() => learnbot(convo));
+                            });
+                        } else convo.say("???").then(() => learnbot(convo));
                     }
                 });
             } else convo.say("???").then(() => learnbot(convo));
