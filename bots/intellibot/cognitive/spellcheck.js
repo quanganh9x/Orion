5e8a5ef41ddad659eec6dee0e663710371adb5a7
quanganh9x <quanganhpro9x@gmail.com>
2018-06-09 21:55:20 +0700

    Add integrity for spell checking

--- bots/intellibot/cognitive/spellcheck.js
+++ bots/intellibot/cognitive/spellcheck.js
@@ -9,8 +9,6 @@ module.exports = (convo, intellibot) => {
         convo.ask('Gửi đoạn văn bản tiếng Anh cần check lên cho mình nhé :x', (payload, convo) => {
             (async () => {
                 const parameters = await {
-                    "mode": "proof",
-                    "mkt": "en-US",
                     "text": payload.message.text
                 };
                 const headers = {};
@@ -20,31 +18,33 @@ module.exports = (convo, intellibot) => {
                 }).then(response => {
                     console.log(response);
                     if (response.flaggedTokens) {
-                        for (let i = 0; i < response.flaggedTokens.length; i++) {
-                            (async () => {
-                                const offset = await response.flaggedTokens[i].offset;
-                                const wrong = await response.flaggedTokens[i].token;
-                                const right = await response.flaggedTokens[i].suggestions[0].suggestion;
-                                await convo.say('Phát hiện lỗi ở vị trí thứ ' + offset + ', chữ: "' + wrong + '" nên thay bằng "' + right);
-                                if (response.flaggedTokens[i].suggestions.length > 1) {
-                                    let orText = '...hoặc thay bằng: ';
-                                    for (let y = 1; y < response.flaggedTokens[i].suggestions.length; y++) {
-                                        if (y === response.flaggedTokens[i].suggestions.length - 1 && y > 1) {
-                                            orText += ' hoặc ';
+                        if (response.flaggedTokens.length !== 0) {
+                            for (let i = 0; i < response.flaggedTokens.length; i++) {
+                                (async () => {
+                                    const offset = await response.flaggedTokens[i].offset;
+                                    const wrong = await response.flaggedTokens[i].token;
+                                    const right = await response.flaggedTokens[i].suggestions[0].suggestion;
+                                    await convo.say('Phát hiện lỗi ở vị trí thứ ' + offset + ', chữ: "' + wrong + '" nên thay bằng "' + right);
+                                    if (response.flaggedTokens[i].suggestions.length > 1) {
+                                        let orText = '...hoặc thay bằng: ';
+                                        for (let y = 1; y < response.flaggedTokens[i].suggestions.length; y++) {
+                                            if (y === response.flaggedTokens[i].suggestions.length - 1 && y > 1) {
+                                                orText += ' hoặc ';
+                                            }
+                                            orText += '"';
+                                            orText += response.flaggedTokens[i].suggestions[y].suggestion;
+                                            orText += '"';
+                                            if (y === response.flaggedTokens[i].suggestions.length - 1) {
+                                                orText += '.';
+                                            } else orText += ',';
                                         }
-                                        orText += '"';
-                                        orText += response.flaggedTokens[i].suggestions[y].suggestion;
-                                        orText += '"';
-                                        if (y === response.flaggedTokens[i].suggestions.length - 1) {
-                                            orText += '.';
-                                        } else orText += ',';
+                                        await convo.say(orText);
                                     }
-                                    await convo.say(orText);
-                                }
-                                if (i === response.flaggedTokens.length - 1) intellibot(convo);
-                            })();
-                        }
-                    } else convo.say('Không check được lỗi ><').then(() => intellibot(convo));
+                                    if (i === response.flaggedTokens.length - 1) intellibot(convo);
+                                })();
+                            }
+                        } else if (response.flaggedTokens.length === 0) convo.say('Không có lỗi sai :D').then(() => intellibot(convo));
+                    } else convo.say('Không check được. Văn bản nhập liệu có lỗi T_T').then(() => intellibot(convo));
                 });
             })();
         });
