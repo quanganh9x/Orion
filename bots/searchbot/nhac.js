a38ce04ab9fc3870fe29eef1ea2224143e4a92fe
quanganh9x <quanganhpro9x@gmail.com>
2018-06-09 23:03:57 +0700

    LLL

--- bots/searchbot/nhac.js
+++ bots/searchbot/nhac.js
@@ -1,4 +1,5 @@
 const request = require('request');
+const querystring =
 const NCT_API = 'https://graph.nhaccuatui.com/v4/';
 
 module.exports = (name, convo, searchbot) => {
@@ -7,6 +8,7 @@ module.exports = (name, convo, searchbot) => {
         headers: {
             'User-Agent': 'NhacCuaTui/6.1.5 (iPhone; iOS 11.1.2; Scale/3.00)',
             'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Length': 1000,
             'x-nct-deviceid': '7B7E319D908841EC98F651F0A75A496C',
             'x-nct-token': 'eyJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1Mjk2NjkyMjQsIm5iZiI6MTUyNzA3NzIyNCwiZGV2aWNlaW5mbyI6IntcIkRldmljZUlEXCI6XCI3QjdFMzE5RDkwODg0MUVDOThGNjUxRjBBNzVBNDk2Q1wiLFwiT3NOYW1lXCI6XCJpT1NcIixcIk9zVmVyc2lvblwiOlwiMTEuMS4yXCIsXCJBcHBOYW1lXCI6XCJOQ1RWNlwiLFwiQXBwVmVyc2lvblwiOlwiNi4xLjVcIixcIlVzZXJOYW1lXCI6XCJhbmhkYW5nMjE5OVwiLFwiUHJvdmlkZXJcIjpcIk5DVENvcnBcIixcIkRldmljZU5hbWVcIjpcImlQaG9uZSA2IFBsdXNcIixcIlF1YWxpdHlQbGF5XCI6XCIxMjhcIixcIlF1YWxpdHlEb3dubG9hZFwiOlwiMTI4XCIsXCJRdWFsaXR5Q2xvdWRcIjpcIjEyOFwiLFwiTmV0d29ya1wiOlwiV0lGSVwiLFwiUGhvbmVOdW1iZXJcIjpcIjE2M3h4eHgzNjNcIixcIkxhbmd1YWdlXCI6XCJWTlwiLFwiQWRJRFwiOlwiN0QyNUY3N0QtMERERS00QkMwLUI2QkItNkIxQjcxMjhBQUM0XCJ9IiwiaWF0IjoxNTI3MDc3MjI0fQ.c9hNdw7hMCEHRWxQPGX_DGJTpRnMpKSF54SbF5Wsyy4',
             'x-nct-version': '6.1.5'
