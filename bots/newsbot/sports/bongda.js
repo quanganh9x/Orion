246403b30697c9cf596c159f5226d00f5c7f946e
Thao <thuthao541998@gmail.com>
2018-06-09 18:46:05 +0700

    sửa

--- bots/newsbot/sports/bongda.js
+++ bots/newsbot/sports/bongda.js
@@ -11,7 +11,7 @@ module.exports = (type, competition, convo, newsbot) => {
     const lastDate = moment(first).add(6, 'days');
     switch (competition) {
         case 'EPL':
-            if (!leagues.epl) {
+            if (leagues.epl == null) {
                 convo.say("Hết giải rồi nên không có thông tin");
             }
             competition = leagues.epl;
