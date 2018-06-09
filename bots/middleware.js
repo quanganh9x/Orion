f73d4aa5a3922915f031c43c6cd46eb1e9893117
quanganh9x <quanganhpro9x@gmail.com>
2018-06-09 20:57:47 +0700

    Argggg

--- bots/middleware.js
+++ bots/middleware.js
@@ -10,7 +10,7 @@ module.exports = function (req, res, next) {
                     await delete req.body.entry[0].messaging[0].postback;
                 }
             } // postback converter
-            if (req.body.entry[0].messaging[0].message && req.body.entry[0].messaging[0].message.nlp.entities && req.body.entry[0].messaging[0].message.text) {
+            if (req.body.entry[0].messaging[0].message && req.body.entry[0].messaging[0].message.nlp && req.body.entry[0].messaging[0].message.nlp.entities && req.body.entry[0].messaging[0].message.text) {
                 if (req.body.entry[0].messaging[0].message.nlp.entities.bots && req.body.entry[0].messaging[0].message.nlp.entities.bots.length !== 0)
                 req.body.entry[0].messaging[0].message.text = req.body.entry[0].messaging[0].message.nlp.entities.bots[0].value;
             }
