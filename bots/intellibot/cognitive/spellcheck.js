3b860883d0da62123d98923ef3f85ec7653b5a90
Tra <trauvmd00621@fpt.edu.vn>
2018-06-09 17:11:18 +0700

    Sửa lại spellcheck không dùng package nữa

--- bots/intellibot/cognitive/spellcheck.js
+++ bots/intellibot/cognitive/spellcheck.js
@@ -1,52 +1,57 @@
-const cognitiveServices = require('cognitive-services');
-const spellcheck = new cognitiveServices.bingSpellCheckV7({
-    apiKey: "2ff21eaca8a94c1da719c24e54289d19",
-    endpoint: "api.cognitive.microsoft.com"
-});
-
-var parameters = {
-    "mode": "proof",
-    "mkt": "en-us"
-};
+const SPELLCHECK_API = 'api.cognitive.microsoft.com/bing/v7.0/spellcheck';
+const apiKey = '2ff21eaca8a94c1da719c24e54289d19';
+const request = require('request');
 
 module.exports = (convo, intellibot) => {
-	(async () => {
-		await convo.say('Chú ý: chưa hỗ trợ check lỗi tiếng Việt T_T');
-	    await convo.ask('Gửi đoạn văn bản tiếng Anh cần check lên cho mình nhé :x', (payload, convo) => {
-	       	convo.set('body', 'text=' + payload.message.text.replace(/ /g, '+'));
-	       	spellCheckClient.spellCheck({
-			    parameters,
-			    body
-			}).then(response => {
-			    if (response.errors) {
-			    	convo.say('Không check được lỗi ><');
-			    	intellibot(convo);
-			    } else {
-			    	for (var i = 0; i < response.flaggedTokens.length; i++) {
-			    		var offset = response.flaggedTokens[i].offset;
-			    		var wrong = response.flaggedTokens[i].token;
-			    		var right = response.flaggedTokens[i].suggestions[0].suggestion;
-			    		await convo.say('Phát hiện lỗi ở vị trí thứ ' + offset + ', chữ: "' + wrong + '" nên thay bằng "' + right);
-			    		// Đoạn dưới này hơi não, cơ mà thôi cứ thêm vào vì người dùng :>
-			    		if(response.flaggedTokens[i].suggestions.length > 1){
-			    			var orText = '...hoặc thay bằng: ';
-			    			for(var y = 1; y < response.flaggedTokens[i].suggestions.length; y++){
-			    				if (y == response.flaggedTokens[i].suggestions.length -1 && y > 1) {
-			    					orText += ' hoặc ';
-			    				}
-			    				orText += '"';
-			    				orText += response.flaggedTokens[i].suggestions[y].suggestion;
-			    				orText += '"';
-			    				if (y == response.flaggedTokens[i].suggestions.length -1) {
-			    					orText += '.';
-			    				} else orText += ',';
-			    			}
-			    			convo.say(orText);
-			    			intellibot(convo);
-			    		}
-			    	}
-			    }
-			});
-	    });
-	})();
+    (async () => {
+        await convo.say('Chú ý: chưa hỗ trợ check lỗi tiếng Việt T_T');
+        convo.ask('Gửi đoạn văn bản tiếng Anh cần check lên cho mình nhé :x', (payload, convo) => {
+            (async () => {
+                const parameters = await {
+                    "mode": "proof",
+                    "mkt": "en-us",
+                    "text": payload.message.text
+                };
+                const headers = await {
+                    'Content-Type' : 'application/x-www-form-urlencoded',
+                    'Content-Length' : payload.message.text.length + 5,
+                    'Ocp-Apim-Subscription-Key' : apiKey
+                };
+                await request.post({
+                    url: SPELLCHECK_API,
+                    headers: headers,
+                    body: JSON.stringify(parameters)
+                }, (error, response, body) => {
+                    if (error) {
+                        convo.say('Không check được lỗi ><').then(() => intellibot(convo));
+                    } else {
+                        for (let i = 0; i < body.flaggedTokens.length; i++) {
+                            (async () => {
+                                const offset = await body.flaggedTokens[i].offset;
+                                const wrong = await body.flaggedTokens[i].token;
+                                const right = await body.flaggedTokens[i].suggestions[0].suggestion;
+                                await convo.say('Phát hiện lỗi ở vị trí thứ ' + offset + ', chữ: "' + wrong + '" nên thay bằng "' + right);
+                                if (body.flaggedTokens[i].suggestions.length > 1) {
+                                    let orText = '...hoặc thay bằng: ';
+                                    for (let y = 1; y < body.flaggedTokens[i].suggestions.length; y++) {
+                                        if (y === body.flaggedTokens[i].suggestions.length - 1 && y > 1) {
+                                            orText += ' hoặc ';
+                                        }
+                                        orText += '"';
+                                        orText += body.flaggedTokens[i].suggestions[y].suggestion;
+                                        orText += '"';
+                                        if (y === body.flaggedTokens[i].suggestions.length - 1) {
+                                            orText += '.';
+                                        } else orText += ',';
+                                    }
+                                    await convo.say(orText);
+                                }
+                                if (i === body.flaggedTokens.length - 1) intellibot(convo);
+                            })();
+                        }
+                    }
+                });
+            })();
+        });
+    })();
 };
