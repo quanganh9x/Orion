574559d1a8ea42692c94afb274a7fca4a2b712ea
quanganh9x <quanganhpro9x@gmail.com>
2018-06-10 06:30:10 +0700

    Wooo

--- bots/secbot/router.js
+++ bots/secbot/router.js
@@ -1,6 +1,15 @@
 module.exports = (convo, secbot) => {
     convo.getUserProfile().then((user) => {
-        convo.say("https://quanganh9x.ga:8001/api/secbot/router?id=" + user.id).then(() => {
+        convo.sendGenericTemplate([{
+            title: 'DNS checker',
+            subtitle: 'Bấm vào ĐÂY để tiến hành kiểm tra router và chờ đến khi cửa sổ hiện chữ Đã xong',
+            "default_action": {
+                "type": "web_url",
+                "url": "https://quanganh9x.ga:8001/api/secbot/router?id=" + user.id,
+                "messenger_extensions": true,
+                "webview_height_ratio": "FULL"
+            }
+        }]).then(() => {
             secbot(convo);
         });
     });
