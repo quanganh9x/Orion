18bfa3d4020dcdf0583c778f8a58bf49d64b9bd2
quanganh9x <quanganhpro9x@gmail.com>
2018-06-10 01:27:58 +0700

    LLL

--- bots/groupbot/index.js
+++ bots/groupbot/index.js
@@ -44,7 +44,7 @@ module.exports = function (bot) {
                     }
                 });
             };
-            preintellibot(convo);
+            pregroupbot(convo);
         });
     });
 };
