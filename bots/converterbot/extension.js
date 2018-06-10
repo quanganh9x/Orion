6790fa62f8d9394e92da73346e5cb6c526064c7f
quanganh9x <quanganhpro9x@gmail.com>
2018-06-10 07:37:15 +0700

    Wooo

--- bots/converterbot/extension.js
+++ bots/converterbot/extension.js
@@ -8,15 +8,17 @@ module.exports = (convo, converterbot) => {
             convo.set("to", payload.message.text);
             convo.ask("Ok. Bạn gửi file lên cho mình nhé ;)", (payload, convo) => {
                 if (payload.message.attachments && payload.message.attachments[0].type) {
-                    let answer = "Đã chuyển: \n";
                     for (let i = 0; i < payload.message.attachments.length; i++) {
                         (async () => {
+                            let answer = "";
                             await request.get({
                                 url: 'https://v2.convertapi.com' + convo.get('from') + '/to/' + convo.get('to') + '?Secret='+ process.env.CONVERT_API_KEY +'&File=' + payload.message.attachments[i].payload.url + '&StoreFile=true',
                                 headers: {
                                     'Accept': 'application/json'
                                 }
                             }, (err, response, body) => {
+                                console.log(body);
+                                body = JSON.parse(body);
                                 if (body && body.Files) {
                                     answer += body.Files[0].Url + "\n";
                                 } else answer += "T_T\n";
