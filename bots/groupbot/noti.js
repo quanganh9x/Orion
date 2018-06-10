d26612ce5432fdd3e2fcfc6a6aecd740cd197fab
quanganh9x <quanganhpro9x@gmail.com>
2018-06-10 08:57:59 +0700

    Release v2.0

--- bots/groupbot/noti.js
+++ bots/groupbot/noti.js
@@ -5,11 +5,13 @@ module.exports = (convo, groupbot, bot) => {
         convo.ask("Gửi thông báo tới nhóm của bạn ?", (payload, convo) => {
             convo.set('noti', payload.message.text);
             Group.findOne({admin: user.id}, (err, result) => {
-                const users = result.id;
-                for (let i = 0; i < users.length; i++) {
-                    bot.say(users[i], convo.get('noti'));
-                    if (i === users.length - 1) convo.say("Hoàn tất!").then(() => groupbot(convo));
-                }
+                if (result) {
+                    const users = result.id;
+                    for (let i = 0; i < users.length; i++) {
+                        bot.say(users[i], convo.get('noti'));
+                        if (i === users.length - 1) convo.say("Hoàn tất!").then(() => groupbot(convo));
+                    }
+                } else convo.say("Bạn chưa đăng ký là Business User!").then(() => groupbot(convo));
             })
         })
     });
